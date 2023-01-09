8dd48d77b6855e235225b6290d881889d7cdfa38
Karthi <karthifairhawn@gmail.com>
2023-01-09 12:46:44 +0530

    added abbrevate number function

--- src/main/webapp/js/util.js
+++ src/main/webapp/js/util.js
@@ -534,3 +534,21 @@ function highlightElement(element,isValid){
     if(isValid) $(element).css('border','1px solid #ced4da');
     else $(element).css('border','1px solid red');
 }
+
+
+ export function abbreviateNumber(num) {
+    const SI_SYMBOL = ['', 'k', 'M', 'G', 'T', 'P', 'E'];
+    const tier = Math.floor(Math.log10(num) / 3) || 0;
+    let result = '' + num;
+    // if zero, we don't need a suffix
+    if (tier > 0) {
+      // get suffix and determine scale
+      const suffix = SI_SYMBOL[tier];
+      const scale = Math.pow(10, tier * 3);
+      // scale the number
+      const scaled = num / scale;
+      // format number and add suffix
+      result = scaled.toFixed(1).replace('.0', '') + suffix;
+    }
+    return "₹ "+result;
+  }
